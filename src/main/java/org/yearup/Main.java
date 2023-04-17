dcdf92f63a00fd21022ec9c52cc09c9afe7dc5fb
Nguon.Z <nzhong@appdev.yearup.org>
2023-04-17 08:24:38 -0500

    annuity update

--- src/main/java/org/yearup/Main.java
+++ src/main/java/org/yearup/Main.java
@@ -1,5 +1,6 @@
 package org.yearup;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
@@ -15,11 +16,15 @@ public class Main {
         double principal;
         double annualInterest;
         int loanLength;
+        double monthlyPayment;
+        double amountOverTime;
+        double totalInterest;
 
 
         // user input
-        System.out.print("Enter Principle: ");
+        System.out.print("Enter Principle Amount: ");
         principal = userInput.nextDouble();
+
 
         System.out.print("Enter Annual Interest Rate: ");
         annualInterest = userInput.nextDouble();
@@ -28,10 +33,79 @@ public class Main {
         System.out.print("Enter loan term: ");
         loanLength = userInput.nextInt();
         loanLength = loanLength * MONTH_IN_YEAR;
-        principal = (principal * annualInterest) / (1 - Math.pow(1 + annualInterest, - loanLength));
+        monthlyPayment = (principal * annualInterest) / (1 - Math.pow(1 + annualInterest, - loanLength));
 
         //output
-        System.out.printf("Monthly Payments: " + principal);
+        System.out.println("Monthly Payments: " + NumberFormat.getCurrencyInstance().format(monthlyPayment));
+
+        amountOverTime = monthlyPayment * loanLength;
+        System.out.println("Total payback amount over time: " + NumberFormat.getCurrencyInstance().format(amountOverTime));
+
+        //A calculator that determines future value of one-time deposit assuming compound interest
+        System.out.println("-------------------------------------------------------------------------------------");
+
+        final int PERCENT1 = 100;
+
+        //variable
+        double deposit;
+        double rate;
+        double term;
+        double compoundInterest;
+        double futureValue;
+
+        System.out.print("Enter your deposit: " );
+        deposit = userInput.nextDouble();
+
+        System.out.print("Enter your interest rate: ");
+        rate = userInput.nextDouble();
+
+        System.out.print("Enter your term: ");
+        term = userInput.nextInt();
+
+        futureValue = deposit * (Math.pow((1 + rate / PERCENT1), term)) - deposit;
+        System.out.println("Future Value: " + NumberFormat.getCurrencyInstance().format(futureValue));
+
+        //Calculator that determines the present value of an ordinary annuity
+        System.out.println("-------------------------------------------------------------------------------------");
+
+        //
+        double paymentAmount;
+        double expectedInterestRate;
+        int years;
+        double presentValue;
+        int payoutPeriod;
+
+
+        //present value = periodic payment
+        //time (time in years) r= annual interest rate
+
+        //user input
+        System.out.print("Enter payment amount: ");
+        paymentAmount = userInput.nextDouble();
+
+        System.out.print("Enter interest rate: ");
+        expectedInterestRate = userInput.nextDouble();
+
+        System.out.print("Enter number of years: ");
+        years = userInput.nextInt();
+
+        //calculate
+        double ratePerPayoutPeriod = expectedInterestRate / MONTH_IN_YEAR;
+        int totalNumPayment = years * MONTH_IN_YEAR;
+        presentValue = paymentAmount * ( ratePerPayoutPeriod / (1 -Math.pow(1 + ratePerPayoutPeriod, -totalNumPayment)));
+
+        //output
+        System.out.println("Present Value: " + NumberFormat.getCurrencyInstance().format(presentValue));
+
+
+
+
+
+
+
+
+
+
 
 
 
